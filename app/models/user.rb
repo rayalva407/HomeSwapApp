91923a95c1201a25ef8544a937a73a494b67b445
rayalva407 <rayalva407@gmail.com>
2020-10-02 17:29:31 -0600

    adds error messages to sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: true
   validates :password, presence: true
   validates :password, confirmation: { case_sensitive: true }
 end
