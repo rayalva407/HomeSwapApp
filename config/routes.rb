d9278fe939f423bd19c5f16c5f0aa43fb1eff6c0
rayalva407 <rayalva407@gmail.com>
2020-10-02 15:41:09 -0600

    adds sessions login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   get 'welcome/start'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   root 'welcome#start'
 end
