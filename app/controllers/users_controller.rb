37857f030c331f2b0b42ccc2436471c2c10321d4
rayalva407 <rayalva407@gmail.com>
2020-10-02 15:16:47 -0600

    add user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def create
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
