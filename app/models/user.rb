259c3bf82dde20141e9425e482992019998b563c
rayalva407 <rayalva407@gmail.com>
2020-10-04 16:02:08 -0600

    adds googleAuth method to userscontroller

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password, presence: true
   validates :password, confirmation: { case_sensitive: true }
+
+  def self.from_omniauth(auth)
+    # Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+    end
+  end
 end
